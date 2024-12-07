4ccd6f24ef29c862fcdffacecc501314ef5ba69d
PriyaS2200 <sonkarpriya667@gmail.com>
2024-12-07 12:50:06 +0530

    feat/arrowfunction

---
+++ q1.js
@@ -0,0 +1,12 @@
+let multiply = (a,b) => {
+    if(a != null && b != null){
+        console.log(a*b)
+    }
+    else{
+        let a = 1;
+        let b = 1;
+        console.log(a*b)
+    }
+}
+multiply();
+multiply(2,3);
